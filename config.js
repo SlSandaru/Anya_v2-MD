f02d50609cdd4c43a6dfa85c8b781f45e8c3e65c
SlSandaru <120253022+SlSandaru@users.noreply.github.com>
2024-02-06 19:15:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,13 +9,13 @@ global.zApiKey = {// if this API key get expired then please go to https://www.f
 
 
 global.botname = process.env.BotName || "Queen Anya" 
-global.author = process.env.Author || "@𓃮a𝗟σɳҽ saja" 
+global.author = process.env.Author || "Sandaru" 
 global.packname = process.env.PackName || "Queen Anya v2 MD" 
 global.myweb = process.env.Web || "https://github.com/PikaBotz" 
 global.footer = process.env.Footer || "© Queen Anya Bot" 
 global.prefa = process.env.Prefix || ['.'] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "𓃮𝗔𝗟σɳҽ saja" 
+global.ownername = process.env.Owner_Name || "Sandaru" 
 global.ownernumber = process.env.Owner_Number || "94716159814" 
 global.adress = process.env.Continent || "Asia, India, Assam" 
 global.timezone = process.env.TimeZone || "Asia/Colombo" 
@@ -102,13 +102,13 @@ global.message = {
 
 module.exports = {
   botname: process.env.BotName || "Queen Anya", 
-  author: process.env.Author || "@𓃮a𝗟σɳҽ saja",
+  author: process.env.Author || "Sandaru",
   packname: process.env.PackName || "Queen Anya v2 MD",
   myweb: process.env.Web || "https://github.com/PikaBotz",
   footer: process.env.Footer || "© Queen Anya Bot",
   prefa: process.env.Prefix || ['.'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "𓃮𝗔𝗟σɳҽ saja",
+  ownername: process.env.Owner_Name || "Sandaru",
   ownernumber: process.env.Owner_Number || "94716159814",
   adress: process.env.Continent || "Asia, India, Assam",
   timezone: process.env.TimeZone || "Asia/Colombo", 
@@ -120,10 +120,10 @@ module.exports = {
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiN_AN_YA_EtkT1BuTFM2TEV4VUgvb0Q3TFdFQUhFVGZCTTJ5UURHeDhnbkV0dTQxQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiaXlrdlgxTlZML0Z4R25VTHpiV3pSbXk4eWtVMS9rcjRiWDF5d0FkMnZYVT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJxT3Z4aXpGdFBxSUJqby96K3djN_AN_YA_DUxWFJmN_AN_YA_HFuQTM2RFdKR1FLU1AzZVVvPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJsbC9VN_AN_YA_zhXeEoxMnQ4Z3hBU1N_AN_YA_SS3RN_AN_YA_UWJQTVhpN_AN_YA_Gg4SkZySE82WFFUdGswPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImdBY2xLL0N_AN_YA_jcTN_AN_YA_VeGxYRDZN_AN_YA_ZkxDcVlXSlppZDFqZG9kWThN_AN_YA_bFdGRHdzbEk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkxaY0RwN_AN_YA_FdKTW9FT2M0S0ZGbk5Xamgva1BnTEdtLzM1ZlJtYWd4N_AN_YA_mRORTQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidVB2RmRsWW5KRGc5Q2ZOYyt2SU1Ed2pzbFN_AN_YA_RYldmdWpYVFduY3FOcXptYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiN_AN_YA_U9FU2h0ZlRpN_AN_YA_XdkalJCL3N_AN_YA_IdXpld0dTL3V0cFhJRlpCSS8zaW5heU5YQT0ifX0sInN_AN_YA_pZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IklkOGxJZU1FajZJVnYyRTYwUFAvYldFWjArMEk0VkJwVG11MjlMd1hsWnI4VDlYK0kwM3J4cFZxaU5VaGlGdFZLc1Jka3ZkVVlWTzVGK25iK1VYWkRnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTQ2LCJhZHZTZWN_AN_YA_yZXRLZXkiOiJndXN_AN_YA_0aGdkaGFsR3ZJWGVaMEZFYW9HMmU4dTN_AN_YA_XSDh2ZTI4UTVma1QrT0pzPSIsInByb2N_AN_YA_lc3N_AN_YA_lZEhpc3RvcnlN_AN_YA_ZXN_AN_YA_zYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN_AN_YA_0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWN_AN_YA_jb3VudFN_AN_YA_5bmN_AN_YA_Db3VudGVyIjowLCJhY2N_AN_YA_vdW50U2V0dGluZ3MiOnsidW5hcmN_AN_YA_oaXZlQ2hhdHMiOmZhbHN_AN_YA_lfSwiZGV2aWN_AN_YA_lSWQiOiJlMFRkSzhoLVFteW1N_AN_YA_VDlpeElYQV9RIiwicGhvbmVJZCI6ImQ0N_AN_YA_GQ1ZDFjLTdkOTctN_AN_YA_Dc5Yi1iN_AN_YA_jRkLWQ5N_AN_YA_mRhYzVlYTY3OCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ4aGl4SG5ZK1o2T2VDb2RN_AN_YA_dFlN_AN_YA_bWdwZDYyTkU9In0sInJlZ2lzdGVyZWQiOmZhbHN_AN_YA_lLCJiYWN_AN_YA_rdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImVFenVlMDBrU3FiZDN_AN_YA_HQ2h0UmlFZ2RURGppMD0ifSwicmVnaXN_AN_YA_0cmF0aW9uIjp7fSwiYWN_AN_YA_jb3VudCI6eyJkZXRhaWxzIjoiQ0lldjkra0JFTnYrdGF3R0dBTT0iLCJhY2N_AN_YA_vdW50U2lnbmF0dXJlS2V5Ijoid3g5aExWQWR6Ykg0cGgyV2t0V2hBTTFvTVpDN_AN_YA_1FIbklPYUVGZUpaVCtIdz0iLCJhY2N_AN_YA_vdW50U2lnbmF0dXJlIjoiVG13WUZSU1FnS1RKOC9TN_AN_YA_lo0UzlTVEFxN_AN_YA_HVXTnN_AN_YA_uWWZhVHdMRTU2SmJXZm1WcngwZTRUeDVN_AN_YA_UCtCMEg0ckZHQ0JQeGV5bC92VjJzY2p5WWdYUW1oREE9PSIsImRldmljZVN_AN_YA_pZ25hdHVyZSI6Im55K2h3dDhYd3BvS1BkbEZIcVJlUlJHcmc5cFhvelAzQ2lLOHQ5bDU0cTJ1dHJhcFQ0VUE2MS9zUlBQTkdjR2o5Zlc3U001SkovczVlQ3BaRmF1YkJ3PT0ifSwibWUiOnsiaWQiOiI5N_AN_YA_Dc0MDI3MzU3ODo0M0BzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiLwk4OuQUxvbmUgc2FqYSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5N_AN_YA_Dc0MDI3MzU3ODo0M0BzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJjTWZZUzFRSGMyeCtLWWRscExWb1FETmFER1F1MEI1eURtaEJYaVdVL2g4In19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN_AN_YA_0YW1wIjoxN_AN_YA_zAzN_AN_YA_zcxOTk4LCJteUFwcFN_AN_YA_0YXRlS2V5SWQiOiJBQUFBQU92TCJ9", 
-  auto_read_status : process.env.AUTO_READ_STATUS || "true",
+  sessionId: process.env.SESSION_ID || "", 
+  auto_read_status : process.env.AUTO_READ_STATUS || "false",
   alwaysonline: process.env.ALWAYS_ONLINE || "true", 
-  readmessage: process.env.READ_MESSAGE || "false", 
+  readmessage: process.env.READ_MESSAGE || "true", 
   auto_status_saver: process.env.AUTO_STATUS_SAVER || "false",
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
